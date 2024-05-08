7da9885665e10f937e92af9e8e8163b033521bd6
zeinab <swarsama@hotmail.co.uk>
2024-05-08 22:46:20 +0100

    working on mongoose.connecting mongodb

--- server/src/index.js
+++ server/src/index.js
@@ -3,12 +3,22 @@ import mongoose from "mongoose";
 import bcrypt from "bcrypt";
 import jsonwebtoken from "jsonwebtoken";
 import cors from "cors";
+import { configDotenv } from "dotenv";
 
 const app = express();
 
 //middleware
 app.use(express.json());
 app.use(cors());
+
+mongoose.connect(process.env.Mongodb)
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((error) => {
+  console.error('Error connecting to MongoDB:', error.message));
+
+// console.log(process.env.Mongodb)
 
 app.listen(3000, () => {
   console.log("Server connected baby!");
